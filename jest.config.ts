7c7b4fc7368548ee35e19c27d49851c1c704bcaa
Abdul <shekhaabdulkadir@gmail.com>
2025-03-29 03:17:27 +0530

    setup unit testing

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+};
+
+export default createJestConfig(config);
